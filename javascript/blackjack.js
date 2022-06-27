942e9611bcd67cc6962ce3a653bc10f2fd5654b8
RotimiFreq <oluwarotimihz@gmail.com>
2022-06-27 14:59:18 +0100

    continued with the project and started adding logic with javascript

---
+++ javascript/blackjack.js
@@ -0,0 +1,23 @@
+// collecting the details of the user 
+
+let v = "sam"
+
+function postdetails() {
+    console.log("v")
+}
+
+
+function firstfunc() {
+    window.location.href = "./gameBJ.html"
+    postdetails()
+
+}
+
+
+// var playername = document.getElementById("playername").value
+// var Deposit = document.getElementById("playerdep").value
+
+// // once details gotten, post it to game page
+
+
+// console.log(v)
